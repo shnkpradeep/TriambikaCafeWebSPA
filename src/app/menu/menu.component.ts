dddd5206396a837bada6b04bf95fc73bd5e0dec8
Pradeep <shnkpradeep@gmail.com>
2026-02-16 03:54:47 +0530

    warnings are fixed

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router'; 
+
 
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
